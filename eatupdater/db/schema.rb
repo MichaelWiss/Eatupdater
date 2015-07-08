d6fe8b96d919309073f7438de050fff938b39312
Michael Wiss <michael.wiss@gmail.com>
2015-07-08 12:10:31 -0500

    user spec, user resource generated, spec

--- eatupdater/db/schema.rb
+++ eatupdater/db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705213201) do
+ActiveRecord::Schema.define(version: 20150708165335) do
 
   create_table "locations", force: true do |t|
     t.decimal  "lat"
     t.decimal  "long"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
