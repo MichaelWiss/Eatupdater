9889546e27f8d5ea076e07b82ac735a550a1d574
Michael Wiss <michael.wiss@gmail.com>
2015-07-11 14:42:29 -0500

    added yelp key, mot getting return working

--- eatupdater/app/controllers/locations_controller.rb
+++ eatupdater/app/controllers/locations_controller.rb
@@ -1,33 +1,65 @@
 class LocationsController < ApplicationController
-   respond_to :js, :json, :html
+     
+
+  def index
+      @locations = Location.all
+  end 
+
+
+
+    
+
+
+
+
+   def show
+      @location = Location.find(params[:id])
+      respond_with @location
+   end
+
+  respond_to :js, :json, :html
    def create
    	    @location = Location.new(location_params)
         respond_to do |format|
             if @location.save
                 format.html { redirect_to @location, notice: 'location was successfully saved.' }
-                format.json { render json: @location, status: :created, location: @product }
+                format.json { render json: @location, status: :created, location: @location }
             else
                 format.html { render action: "new" }
             end 
         end
-    end
+  
+   
+ include Yelp::V2::Search::Request
+   def search
+     client = Yelp::Client.new
 
-   def show
-  	@location = Location.find(params[:id])
-  	respond_with @location
+     request = location.new(
+                 :term => 'thai',
+                 :category_filter => 'food,restaurants',
+                 :city=> params[:city],
+                 :limit => 20,
+                 :radius_filter => 8047,
+                 :latitude => params[:lat],
+                 :longitude => params[:long])
+     response = client.search(request)
+     render json: response
+
+     # rest of your code
    end
 
-   def index
-    @locations = @location.all
 
-   end
+  
+   
+
+   
 
 
+  
   private
-      def location_params
-      params.require(:location).permit(:lat, :long)
-  end
-
-
+    def location_params
+      params.require(:location).permit(:lat, :long, :name, :address,)
+    end
+end
 
 end
---
+++ eatupdater/config/initializers/yelp.rb
@@ -0,0 +1,4 @@
+Yelp.configure(:consumer_key    => 'zZUWULzB1vga0IjnfLaPlw',
+               :consumer_secret => 'dzFGvbHaBF6wEWazUaXxazQFl3M',
+               :token           => '6rixeDCI0WivE69sygpY_T9MVUoOfCDE',
+               :token_secret    => 'im7qk3QIIfUISyOJYfHuwjVTbhA')
