40caf5097284fe4d195857fdb402840b69695d8f
Michael Wiss <michael.wiss@gmail.com>
2015-07-11 19:17:11 -0500

    messed something up, controller doesn't like params

--- eatupdater/app/controllers/locations_controller.rb
+++ eatupdater/app/controllers/locations_controller.rb
@@ -1,64 +1,35 @@
 class LocationsController < ApplicationController
      
-
+require 'yelp'
   def index
       @locations = Location.all
   end 
 
-
-
-    
-
-
-
-
-   def show
+ def show
       @location = Location.find(params[:id])
       respond_with @location
    end
 
   respond_to :js, :json, :html
    def create
-   	    @location = Location.new(location_params)
+   	    @location = Location.new(location_params[:lat, :long])
         respond_to do |format|
             if @location.save
-                format.html { redirect_to @location, notice: 'location was successfully saved.' }
-                format.json { render json: @location, status: :created, location: @location }
+                
+                render json: { status: 'success' }
             else
-                format.html { render action: "new" }
+                render json: { status: 'failure' }
             end 
         end
-  
-   
- include Yelp::V2::Search::Request
-   def search
-     client = Yelp::Client.new
-
-     request = location.new(
-                 :term => 'thai',
-                 :category_filter => 'food,restaurants',
-                 :city=> params[:city],
-                 :limit => 20,
-                 :radius_filter => 8047,
-                 :latitude => params[:lat],
-                 :longitude => params[:long])
-     response = client.search(request)
-     render json: response
-
-     # rest of your code
-   end
-
 
   
-   
-
-   
 
 
-  
+
+
   private
     def location_params
-      params.require(:location).permit(:lat, :long, :name, :address,)
+      params.permit(:lat, :long)
     end
 end
 
--- eatupdater/config/initializers/yelp.rb
+++ eatupdater/config/initializers/yelp.rb
@@ -1,4 +1,6 @@
-Yelp.configure(:consumer_key    => 'zZUWULzB1vga0IjnfLaPlw',
-               :consumer_secret => 'dzFGvbHaBF6wEWazUaXxazQFl3M',
-               :token           => '6rixeDCI0WivE69sygpY_T9MVUoOfCDE',
-               :token_secret    => 'im7qk3QIIfUISyOJYfHuwjVTbhA')
+Yelp.client.configure do |config|
+	config.consumer_key = 'zZUWULzB1vga0IjnfLaPlw'
+	config.consumer_secret = 'dzFGvbHaBF6wEWazUaXxazQFl3M'
+	config.token = '2Q7AUTS1u9j3-61oR3j6OEzJZhL9Vf2E'
+	config.token_secret = 'cwcWJmOT4pN7f8YCOgvEIxdLcfY'
+end
