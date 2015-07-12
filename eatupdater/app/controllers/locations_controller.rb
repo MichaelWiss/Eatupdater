7ce4840d81c40f1c6801ae8d02d8ceff420e34b0
Michael Wiss <michael.wiss@gmail.com>
2015-07-12 16:28:33 -0500

    params problems

--- eatupdater/app/controllers/locations_controller.rb
+++ eatupdater/app/controllers/locations_controller.rb
@@ -1,33 +1,30 @@
 class LocationsController < ApplicationController
  respond_to :js, :json, :html
 
-  def new
-    @location = Location.new
-  end
-
   def index
       @locations = Location.all
   end 
 
   def show
-      @location = Location.find(location_params[:id])
+      @location = Location.find(params[:id])
       respond_with @location
   end
 
   def create
-            @location = Location.new(location_params)
-            respond_to do |format|
+       @location = Location.new(location_params[:location])
+        respond_to do |format|
             if @location.save
-                format.html {redirect_to root_url, :notice => "Signed up!"}
+              puts "We’re in the IF statement"
+                format.html {redirect_to @user, notice: "Thanks for signing up!"}
             else
-                format.html {render :new}
+             format.html {render :new}
             end
   end
 
 private
 
   def location_params
-      params.require(:location).permit(:lat, :long, :id)
+      params.require(:location).permit(:lat, :long)
   end
 end
 end
