361b6297ac5161ca4824759d4fe4653c0533a4e4
Michael Wiss <michael.wiss@gmail.com>
2015-08-02 15:22:19 -0500

    user create action

--- eatupdater/config/routes.rb
+++ eatupdater/config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   root 'pages#about'
 
+  get 'signup' => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
