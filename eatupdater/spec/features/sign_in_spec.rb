3aecab6468addffa83a7413e19f54ad965291183
Michael Wiss <michael.wiss@gmail.com>
2015-08-05 14:52:43 -0500

    fixing rspec tests

--- eatupdater/spec/features/sign_in_spec.rb
+++ eatupdater/spec/features/sign_in_spec.rb
@@ -1,3 +1,22 @@
+require 'spec_helper'
+
+describe "Signing in" do
+
+  it "prompts for an email and password" do
+    visit root_url
+
+    click_link 'Sign In'
+
+  
+
+    expect(page).to have_field("Email")
+    expect(page).to have_field("Password")
+  end
+
+
+
+
+
 it "signs in the user if the email/password combination is valid" do
   user = User.create!(user_attributes)
 
@@ -13,6 +32,9 @@ it "signs in the user if the email/password combination is valid" do
   expect(current_path).to eq(user_path(user))   
 
   expect(page).to have_text("Welcome back, #{user.name}!")
+   expect(page).to have_link(user.name)
+  expect(page).not_to have_link('Sign In')
+  expect(page).not_to have_link('Sign Up')
 end
 
 it "does not sign in the user if the email/password combination is invalid" do
@@ -28,20 +50,10 @@ it "does not sign in the user if the email/password combination is invalid" do
   click_button 'Sign In'
 
   expect(page).to have_text('Invalid')
-end
-
-it "signs in the user if the email/password combination is valid" do
-  # existing code
-
-  expect(page).to have_link(user.name)
-  expect(page).not_to have_link('Sign In')
-  expect(page).not_to have_link('Sign Up')
-end
-
-it "does not sign in the user if the email/password combination is invalid" do
-  # existing code
-
   expect(page).not_to have_link(user.name)
   expect(page).to have_link('Sign In')
   expect(page).to have_link('Sign Up')
 end
+end
+
+
