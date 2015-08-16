22a504ae7b9fa10e4f77d344a316fc096f4088a2
Michael Wiss <michael.wiss@gmail.com>
2015-08-16 12:56:25 -0500

    get rid of edit/update links for non matching user

--- eatupdater/app/controllers/application_controller.rb
+++ eatupdater/app/controllers/application_controller.rb
@@ -15,5 +15,9 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_user?(user)
+      current_user == user
+  end
+  helper_method :current_user?
   helper_method :current_user
 end
