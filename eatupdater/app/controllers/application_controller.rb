64864ef0ef051eda0b2c68c079374782323c2fe5
Michael Wiss <michael.wiss@gmail.com>
2015-08-16 13:46:49 -0500

    fixed bug in user show

--- eatupdater/app/controllers/application_controller.rb
+++ eatupdater/app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::Base
   def current_user?(user)
       current_user == user
   end
-  helper_method :current_user?
+  
   helper_method :current_user
+  helper_method :current_user?
 end
