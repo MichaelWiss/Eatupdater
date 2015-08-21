5b73cff7ccc4689759f190d74cc40b1a8b65bccf
Michael Wiss <michael.wiss@gmail.com>
2015-08-21 13:38:55 -0500

    experimenting with off canvas nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,15 @@ $(function(){
   });
 });
 
-$alert-transition-speed: 300ms;
+
+
+
+
+$(function() {
+	$(".right-off-canvas-toggle, .exit-off-canvas").click(function() {
+    	$(".top-bar").toggleClass("top-bar-close");
+		$(".middle-bar").toggleClass("middle-bar-close");
+		$(".bottom-bar").toggleClass("bottom-bar-close");
+  	});
+});
+
