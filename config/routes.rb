2488a54d8e1483225c55bef367189834093e1693
Michael Wiss <michael.wiss@gmail.com>
2015-09-13 19:35:12 -0500

    routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get 'static_pages/about', :as => 'about'
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', :as => 'contact'
 
   resources :users
 
